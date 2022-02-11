8dc3f77cca1d76b4a426077dedccb5e61800e1df
Danspotnytool <63223301+Danspotnytool@users.noreply.github.com>
2022-02-11 12:47:53 +0800

    fix indetation

--- stayAwake.js
+++ stayAwake.js
@@ -4,12 +4,12 @@ const app = express();
 const uptime = Date.now();
 
 app.get('/', (req, res) => {
-  res.send({
-    status: "online",
-    uptime: Date.now() - uptime
-  });
+    res.send({
+        status: "online",
+        uptime: Date.now() - uptime
+    });
 });
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log(`Listening to port: ${process.env.PORT || 8080}`);
+    onsole.log(`Listening to port: ${process.env.PORT || 8080}`);
 });
