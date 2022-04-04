c626f4f95d4d57db532799461d57de34f87c8604
Danspotnytool <63223301+Danspotnytool@users.noreply.github.com>
2022-04-04 14:05:07 +0800

    automatic rejoin on restart

--- index.js
+++ index.js
@@ -4,25 +4,41 @@ require('dotenv').config();
 const logger = require('./logger.js');
 
 const Discord = require('discord.js-selfbot');
+const fs = require('fs');
+const sqlite3 = require('sqlite3').verbose();
+
+// Check if './database' folder exists, if not create it
+if (!fs.existsSync('./database')) {
+    fs.mkdirSync('./database');
+};
+// Check if './database/bot.db' file exists, if not create it
+if (!fs.existsSync('./database/bot.db')) {
+    fs.openSync('./database/bot.db', 'w');
+};
 
 const bots = [
     `${process.env.TOKEN1}`,
-    `${process.env.TOKEN2}`,
-    `${process.env.TOKEN3}`,
-    `${process.env.TOKEN4}`,
-    `${process.env.TOKEN5}`,
-    `${process.env.TOKEN6}`,
-    `${process.env.TOKEN7}`,
-    `${process.env.TOKEN8}`,
-    `${process.env.TOKEN9}`,
-    `${process.env.TOKEN10}`,
-    `${process.env.TOKEN11}`,
-    `${process.env.TOKEN12}`,
 ];
 
 require('./stayAwake.js');
 
 const timestamp = Date.now();
+
+// Check if bot table exist. If not, create it
+const db = new sqlite3.Database('./database/bot.db', (err) => {
+    if (err) {
+        console.error(err.message);
+    };
+    // Only create table for the botID and their VC ID
+    db.run(`CREATE TABLE IF NOT EXISTS bot (
+        botID TEXT,
+        vcID TEXT
+    )`, (err) => {
+        if (err) {
+            console.error(err.message);
+        };
+    });
+});
 
 bots.forEach((token) => {
 
@@ -30,6 +46,38 @@ bots.forEach((token) => {
     client.on('ready', () => {
         console.log(logger.blue('Bot is ready!'), logger.green(`Logged in as ${client.user.tag}!`));
         // client.user.setMute(true);
+
+        // Check if bot is in the database
+        db.get(`SELECT * FROM bot WHERE botID = '${client.user.id}'`, (err, row) => {
+            if (err) {
+                console.error(err.message);
+            };
+            // If bot is not in the database, add it
+            if (!row) {
+                db.run(`INSERT INTO bot (botID, vcID) VALUES ('${client.user.id}', 'null')`, (err) => {
+                    if (err) {
+                        console.error(err.message);
+                    };
+                });
+            };
+            // If bot is in the database, check if it has a VC ID
+            if (row) {
+                console.log(row);
+                if (row.vcID == 'null') { return };
+                if (row.vcID == 'undefined') { return };
+                if (row.vcID == null) { return };
+
+                try {
+                    client.channels.cache.get(row.vcID).join();
+                } catch(err) {
+                    db.run(`UPDATE bot SET vcID = 'null' WHERE botID = '${client.user.id}'`, (err) => {
+                        if (err) {
+                            console.error(err.message);
+                        };
+                    });
+                };
+            };
+        });
     });
 
     let currentVC = null;
@@ -70,6 +118,14 @@ bots.forEach((token) => {
                     // Join the voice channel
                     await userVC.join().then((connection) => {
                         currentVC = connection;
+                        // Get the VC ID
+                        const vcID = userVC.id;
+                        // Update the database with the VC ID
+                        db.run(`UPDATE bot SET vcID = '${vcID}' WHERE botID = '${client.user.id}'`, (err) => {
+                            if (err) {
+                                console.error(err.message);
+                            };
+                        });
                         msg.channel.send(`Joined ${userVC.name}`)
                             .then((message) => {
                                 setTimeout(() => {
@@ -93,6 +149,14 @@ bots.forEach((token) => {
             // Join the voice channel
             await client.channels.cache.get(args[0]).join().then((connection) => {
                 currentVC = connection;
+                // Get the VC ID
+                const vcID = args[0];
+                // Update the database with the VC ID
+                db.run(`UPDATE bot SET vcID = '${vcID}' WHERE botID = '${client.user.id}'`, (err) => {
+                    if (err) {
+                        console.error(err.message);
+                    };
+                });
                 msg.channel.send(`Joined ${args[0]}`)
                     .then((message) => {
                         setTimeout(() => {
@@ -122,6 +186,12 @@ bots.forEach((token) => {
                             message.delete();
                         }, 5000);
                     });
+                // Update the database with the VC ID
+                db.run(`UPDATE bot SET vcID = 'null' WHERE botID = '${client.user.id}'`, (err) => {
+                    if (err) {
+                        console.error(err.message);
+                    };
+                });
             } else {
                 msg.channel.send('Not in a voice channel')
                     .then((message) => {
@@ -135,12 +205,20 @@ bots.forEach((token) => {
 
     // Listen to when the client is moved to diffent voice channel
     client.on('voiceStateUpdate', (oldMember, newMember) => {
-        try {
-            currentVC = newMember.voice.channel;
-        } catch (err) {};
-        try {
-            currentVC = oldMember.voice.channel;
-        } catch (err) {};
+        // Check if this user is the bot
+        if (newMember.id != client.user.id) { return };
+        if (oldMember.id != client.user.id) { return };
+        // Check if the bot is in a voice channel
+        if (newMember) {
+            // Get the VC ID
+            const vcID = newMember.channelID;
+            // Update the database with the VC ID
+            db.run(`UPDATE bot SET vcID = '${vcID}' WHERE botID = '${client.user.id}'`, (err) => {
+                if (err) {
+                    console.error(err.message);
+                };
+            });
+        };
     });
 
     client.login(token);
